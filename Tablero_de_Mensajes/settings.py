7a84e6e657b2c8feb8c816e5ac49bb1e9bc033a9
Boga <enriquearmandobogarin@gmail.com>
2024-09-08 14:09:22 -0300

    Modelo de mensaje y creacion de las urls de la app y carpeta templates

--- Tablero_de_Mensajes/settings.py
+++ Tablero_de_Mensajes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mensajes',     #La app mensajes
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Tablero_de_Mensajes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],  #Carpeta de templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mensajes/models.py
+++ mensajes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Mensaje(models.Model):
+    remitente = models.CharField(max_length=100)
+    destinatario = models.CharField(max_length=100)
+    texto = models.TextField()
+    fecha_hora = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.remitente} a {self.destinatario}: {self.texto}"
+
+    @staticmethod
+    def filtrar_por_destinatario(destinatario):
+        return Mensaje.objects.filter(destinatario=destinatario).order_by('fecha_hora')
